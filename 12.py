04794bd038164ba5eb4963e620de9a7f1619d762
buddyuchiha <Pro100biv@mail.ru>
2025-08-02 00:21:45 +0400

    added work with file conspects and asyncio information

--- 12.py
+++
@@ -1,7 +0,0 @@
-a = [(1, '{"id": 1, "name": "USD"', 2), ('b', '{"id": 1, "name": "USD"', 2)]
-result = []
-for i in a:
-    res = dict(i)
-    result.append(res)
-
-print(result)
---
+++ tools/06.py
@@ -0,0 +1,6 @@
+import asyncio 
+
+async def example():
+    print(100)
+    
+print(example())
